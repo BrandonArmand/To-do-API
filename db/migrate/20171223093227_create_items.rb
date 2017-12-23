f29bd444992172fc4c700c71b54099a84a82fd19
brandon armand <bwelsh8@gmail.com>
2017-12-23 10:42:26 +0000

    created models, and serialized them

---
+++ db/migrate/20171223093227_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.references :list, index: true
+      t.string :description
+      t.boolean :completed, default: false
+
+      t.timestamps null: false
+    end
+  end
+end
